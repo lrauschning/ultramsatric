b8c125991b409efbad152374eb6017d87b64afbc
Leon Rauschning <rauschning@cip.ifi.lmu.de>
2023-10-11 18:15:46 +0200

    played around with packaging, now works with both setup.py and pip

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 #/usr/bin/env python3
 from setuptools import setup
 
-setup()
+setup(entry_points={"console_scripts": ["ultramsatric=ultramsatric:main"]})
--- ultramsatric/__init__.py
+++ ultramsatric/__init__.py
@@ -1,1 +1,6 @@
 __version__ = "0.1"
+
+from .main import main
+
+if __name__ == '__main__':
+    main()
