a9113d9cfdc82a90e9572d57553730f0d59b4ece
Leon Rauschning <rauschning@cip.ifi.lmu.de>
2023-10-26 16:54:22 +0200

    testing method in main

--- ultramsatric/main.py
+++ ultramsatric/main.py
@@ -1,5 +1,20 @@
-from . import __version__
+#!/bin/env python3
+#from . import __version__
+import sys
+
+from msa import MSA
+from distance import *
 
 def main():
     print("Hello world!")
-    print(__version__)
+    #print(__version__)
+
+    print(sys.argv)
+    m = MSA.from_file(sys.argv[1])
+    print(m)
+    d = DistMat.from_msa(m, distfun=alignment_distance)
+    print(d)
+
+
+if __name__ == "__main__":
+    main()
