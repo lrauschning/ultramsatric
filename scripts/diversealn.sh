c61cba2cb0048df72db51daa87bd3c9379d32510
Leon Rauschning <99650940+lrauschning@users.noreply.github.com>
2024-03-26 17:04:35 +0100

    update diversealn.sh to sample 10 sequences and be more robust

--- scripts/diversealn.sh
+++ scripts/diversealn.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 
+mamba init
+mamba activate tcoffee
+
+export DUMP_SEQ_BUCKETS_ONLY=1
 for x in blmb lyase_1 p450 rvp; do
       # output in the right dir
       mkdir $x
       cd $x
 
-      # get the 50 most diverse sequences
-      export DUMP_SEQ_BUCKETS_ONLY=1
-      t_coffee -reg -thread 10 -seq ~/homfam/combinedSeqs/${x}.fa -nseq 50
+      # get the 10 most diverse sequences as the first seqdump
+      t_coffee -reg -thread 4 -seq ~/homfam/combinedSeqs/${x}.fa -nseq 10
 
-      # align them
-      export DUMP_SEQ_BUCKETS_ONLY=0
-      t_coffee -thread 10 -seq ./seqdump.1 -output fasta_aln -outfile diverse.aln.fa
+		mv $x/seqdump.1 ./${x}.div.fa
 
       cd ..
+done
+
+export DUMP_SEQ_BUCKETS_ONLY=0
+for x in ./*.div.fa; do
+      # align them using tcoffee
+		t_coffee -thread 4 -seq $x -output fasta_aln -outfile $(basename $x .fa).tcof.fa
+		#TODO align using msa
+		# ~/msa -i ...
 done
