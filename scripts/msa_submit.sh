4c70ede06634c5e8cfde73a95d2fec48303db083
Leon Rauschning <99650940+lrauschning@users.noreply.github.com>
2024-03-27 16:00:20 +0100

    prettify logging

--- scripts/msa_submit.sh
+++ scripts/msa_submit.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 for x in ./*.div.fa; do
-	echo Submitting ./msacall.sh $x
+	echo Submitting ./msa_call.sh $x
 	#./submit.sh ~/ultramsatric/scripts/msa_call.sh $x
+	echo Submitting ./msa_mutatedcall.sh $x
 	./submit.sh ~/ultramsatric/scripts/msa_mutatedcall.sh $x
 done
