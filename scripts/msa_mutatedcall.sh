2d3d56c82c1d63927d95684622c1160bdd6bbff2
Leon Rauschning <99650940+lrauschning@users.noreply.github.com>
2024-03-27 16:02:25 +0100

    whoops, fix conflicting filename

--- scripts/msa_mutatedcall.sh
+++ scripts/msa_mutatedcall.sh
@@ -2,4 +2,4 @@
 
 cd diversealn
 
-~/carlip/msa -c ~/ultramsatric/scripts/mutated.scores $1 | ~/ultramsatric/scripts/msatofasta.py $1 > ~/diversealn/$(basename $1 .fa).opt.aln
+~/carlip/msa -c ~/ultramsatric/scripts/mutated.scores $1 | ~/ultramsatric/scripts/msatofasta.py $1 > ~/diversealn/$(basename $1 .fa).mut.aln
