18c459f22686b3779260af688ee78cfcaf65031f
Leon Rauschning <99650940+lrauschning@users.noreply.github.com>
2024-03-28 18:28:39 +0100

    whoops, fix mutatedcall

--- scripts/msa_mutatedcall.sh
+++ scripts/msa_mutatedcall.sh
@@ -5,5 +5,5 @@ cd diversealn
 bn=$(basename $1 .fa)
 
 # piping seems to fail on long jobs on the cluster, so write the output to a file
-~/carlip/msa -c ~/ultramsatric/scripts/mutated.scores $1 > ~/diversealn/$bn.opt.out
-cat ~/diversealn/$bn.opt.out | ~/ultramsatric/scripts/msatofasta.py $1 > ~/diversealn/$bn.opt.aln.fa
+~/carlip/msa -c ~/ultramsatric/scripts/mutated.scores $1 > ~/diversealn/$bn.mut.out
+cat ~/diversealn/$bn.opt.out | ~/ultramsatric/scripts/msatofasta.py $1 > ~/diversealn/$bn.mut.aln.fa
