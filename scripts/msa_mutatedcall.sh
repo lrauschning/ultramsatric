1bb5e061d76e6dff4cc0ee6dab96333c09d09d37
Leon Rauschning <99650940+lrauschning@users.noreply.github.com>
2024-09-24 10:48:20 +0200

    distintct name for MUT

--- scripts/msa_mutatedcall.sh
+++ scripts/msa_mutatedcall.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-user=l.rauschning@campus.lmu.de
 #SBATCH --output=logs/msa-mutated-%j.out
 #SBATCH --error=logs/msa-mutated-%j.err
-#SBATCH --job-name=CARLIP_MSA
+#SBATCH --job-name=CARLIP_MSA_MUT
 #SBATCH -M biohpc_gen
 #SBATCH --partition=biohpc_gen_production
 #SBATCH --time=3-00:00:00
